949b5bc5a1e037a1570b794858eb9fcd0bc88e7b
moustafaramadan <moustafaramadan15@gmail.com>
2026-06-19 09:25:48 +0300

    Remove contact map and fix contact social icon behavior

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -210,23 +210,6 @@ function Contact() {
         </div>
       </section>
 
-      {/* Map Section */}
-      <section className="py-lg-8 py-5 contact-map">
-        <div className="container-fluid px-0">
-          <div className="map-container">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3024.1234567890!2d-74.0060!3d40.7128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDDCsDQyJzQ2LjEiTiA3NMKwMDAnMjEuNiJX!5e0!3m2!1sen!2sus!4v1234567890"
-              width="100%"
-              height="400"
-              style={{ border: 0 }}
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-              title="Furnish Store Location"
-            ></iframe>
-          </div>
-        </div>
-      </section>
     </div>
   );
 }
